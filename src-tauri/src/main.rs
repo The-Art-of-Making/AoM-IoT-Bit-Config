0838974e581f2fc74e93cdd2de6ed433ad52b2f5
delta-12 <btesq235@gmail.com>
2023-01-25 10:59:41 -0500

    fs testing in main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,28 @@
     windows_subsystem = "windows"
 )]
 
+use std::fs;
+use tauri::scope::FsScope;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn allow_file_wrapper(path: String) {
+    println!("File path {}", path);
+    // FsScope::allow_file(FsScope, path);
+
+    // println!("In file {}", path);
+    // let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+    // println!("With text:\n{contents}");
+}
+
+#[tauri::command]
+fn read_file(path: String) -> String {
+    fs::read_to_string(path).expect("Should have been able to read the file")
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![allow_file_wrapper])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
